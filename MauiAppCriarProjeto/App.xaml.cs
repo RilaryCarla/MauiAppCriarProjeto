a7c759e3718aebf11b8d9829867095d5e7f5f403
Usuario <Usuario@CPD03-M11>
2024-02-26 14:06:06 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppCriarProjeto/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiAppCriarProjeto
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
